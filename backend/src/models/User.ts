cd72e8bd198b7f81b796ebdd29ce3abea541ef37
eitaneitan7 <eitaneitan7@gmail.com>
2024-04-06 23:47:10 +0300

    fixed user type

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,5 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 import bcrypt from 'bcryptjs';
+
+interface IUser extends Document {
+    username: string;
+    password: string;
+    email: string;
+    comparePassword: (candidatePassword: string) => Promise<boolean>;
+  }
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -7,18 +14,19 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
 });
 
-// explain .pre , and this entire functions
-userSchema.pre('save', async function (next) {
+// before inserting document check for pass encryption
+userSchema.pre<IUser>('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 8);
   next();
 });
 
-//explain please entire function
+
+// compare userpass with DB pass
 userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
   return bcrypt.compare(candidatePassword, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model<IUser>('User', userSchema);
 
 export default User;
